02062edc1502ba4fe0683086f3e3d16f1f27dbf8
Tiemi Imayoshi <tiemi.imayoshi@avanade.com>
2024-05-03 16:25:44 -0300

    Segundo commit

---
+++ Controle de vendas/Controllers/NivelAcessoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controle_de_vendas.Controllers
+{
+    public class NivelAcessoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Controle de vendas/obj/Debug/net8.0/Controle de vendas.AssemblyInfo.cs
+++ Controle de vendas/obj/Debug/net8.0/Controle de vendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Controle de vendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6df78580836db3fde191777388ec98adf014e83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Controle de vendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Controle de vendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
